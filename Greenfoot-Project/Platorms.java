e61a6c64085a959392fcf5723b046a5678e9b6f8
Xuerui Fa <xuerui.fa@hotmail.com>
2015-02-09 17:10:00 -0500

    Recommit

--- Greenfoot-Project/Platorms.java
+++ Greenfoot-Project/Platorms.java
@@ -1,21 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class NonvectorEntity here.
+ * Write a description of class Platorms here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class NonvectorEntity extends Actor
+public class Platorms extends NonvectorEntity
 {
     /**
-     * Act - do whatever the NonvectorEntity wants to do. This method is called whenever
+     * Act - do whatever the Platorms wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
         // Add your action code here.
     }    
-    
-    
 }
--- readme.txt
+++ readme.txt
@@ -1,4 +1,2 @@
-﻿<<<<<<< HEAD
-Hi Alex I am communicating to you via GitHub wow this is cool I am much excite
-
 ﻿Alex Maynard and Xuerui Fa’s semester project for AP Computer Science: a game developed through Greenfoot.
+>>>>>>> origin/master
