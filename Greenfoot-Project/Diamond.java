215cebd2aa8860a12017f852f475dd13f14d8964
Alex Maynard <alex.j.maynard@gmail.com>
2015-02-11 16:45:35 -0500

    Added TorqueEntity class
    Allows the creation of objects whose vectors affect its rotation. Right
    now VectorEntity and TorqueEntity are mutually exclusive, but they
    could potentially be combined in the future if necessary. An
    application of the class can be seen with the diamonds. When
    instantiated, they sway back and forth. Movement isn’t as smooth as it
    could be, will fix later.

--- Greenfoot-Project/Diamond.java
+++ Greenfoot-Project/Diamond.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Diamond extends NonvectorEntities
+public class Diamond extends TorqueEntity
 {
     /**
      * Act - do whatever the Diamond wants to do. This method is called whenever
@@ -14,13 +14,26 @@ public class Diamond extends NonvectorEntities
      */
     public void act() 
     {
+        super.act();
         checkForPlayer();
-    } 
+        diamondMovement();
+    }
+    public Diamond() {
+        setRotation(15);
+    }
     private void checkForPlayer() { //Checks to see if touching the player
         Actor player = (Actor) getOneObjectAtOffset(0, 0, TestMan.class);
-        if (player!=null) {
+        if (player!=null) { //If there's a player touching the diamond, the score is incremented and the diamond is removed
             Scorekeeper.incrementDiamonds();
             getWorld().removeObject(this);
+        }
+    }
+    public void diamondMovement() {
+        if (getRotation()>0 && getRotation()<90) {
+            addTorque(0.15);
+        }
+        if (getRotation()>270 && getRotation()<359) {
+            subtractTorque(0.15);
         }
     }
 }
