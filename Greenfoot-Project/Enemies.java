26e031337e7a2fbd26a20899a9703932af239e8d
Xuerui Fa <xuerui.fa@hotmail.com>
2015-02-17 10:30:33 -0500

    Trying stuff out

--- Greenfoot-Project/Enemies.java
+++ Greenfoot-Project/Enemies.java
@@ -25,25 +25,25 @@ public class Enemies extends VectorEntity
         enemyMovement();
     }   
     
-    //public void move() //Moves enemy back and forth
-    //{
-    //    Actor ground = getOneObjectAtOffset(checkX, checkY, Platform.class);
-    //    if(ground == null)
-    //    {
-    //        speed *= -1; // Reverses direction
-    //        checkX*= -1; // Looks for a negative number
-    //    }
-    //    else
-    //    {
-    //        move(speed);
-    //    }
-    //}
+    public void move() //Moves enemy back and forth
+    {
+        Actor ground = getOneObjectAtOffset(checkX, checkY, Platform.class);
+        if(ground == null)
+        {
+            speed *= -1; // Reverses direction
+            checkX*= -1; // Looks for a negative number
+        }
+        else
+        {
+            move(speed);
+        }
+    }
     
-    //public void addVector(double xValue, double yValue)
-    //{
-    //    super.addVector(xValue, yValue);
-    //    setLocation((int)this.getXComp(), (int)this.getYComp());
-    //}
+    public void addVector(double xValue, double yValue)
+    {
+        super.addVector(xValue, yValue);
+        setLocation((int)this.getXComp(), (int)this.getYComp());
+    }
     
     public void enemyMovement() {
         Actor platform = getOneObjectAtOffset(0, checkY, Platform.class);
