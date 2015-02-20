c050c83e175f3becefb265449c0310c9216744fe
Alex Maynard <alex.j.maynard@gmail.com>
2015-02-20 15:52:08 -0500

    Animated Enemy
    The enemy has a walking animation now.

--- Greenfoot-Project/Enemy.java
+++ Greenfoot-Project/Enemy.java
@@ -11,6 +11,10 @@ public class Enemy extends BetterActor
     Force force = new Force();
     private int speed = 2;
     private double healthEnemy = 2;
+    private GreenfootImage walking1 = new GreenfootImage("Alien.Still.png");
+    private GreenfootImage walking2 = new GreenfootImage("Alien.Walking.png");
+    private int moveTime = 1; //Iterator for the animation of the enemy's movement
+    
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -22,12 +26,13 @@ public class Enemy extends BetterActor
         force.lookForWall(this);
         force.lookForCeiling(this);
         enemyMovement();
+        enemyAnimation();
     }
     public Enemy() {
-        rightExcess = 16;
-        leftExcess = 15;
-        botExcess = 2;
-        topExcess = 1;
+        rightExcess = 0;
+        leftExcess = 0;
+        botExcess = 4;
+        topExcess = 0;
     }
     public void enemyMovement() { //Describes how the enemy moves
         move(speed);
@@ -41,11 +46,22 @@ public class Enemy extends BetterActor
         if (force.isTouchingWall(this) || (!(left&&right)&&(left||right))) { //If touching a wall, the enemy turns. If there's a platform below it on one
             move(-speed);                                                    //side, but not the other, it turns as well. This uses an XOR gate.
             speed*=-1;
+            walking1.mirrorHorizontally();
+            walking2.mirrorHorizontally();
         }
     }
     public void decrementPlayersHealth() {
         if (force.canSee(Player.class, this)) {
             Player.setHealth(Player.getHealth()-0.5);
         }
+    }
+    public void enemyAnimation() { //Animates the enemy's movement
+        if (moveTime==10)
+            setImage(walking2);
+        if (moveTime>=20) {
+            setImage(walking1);
+            moveTime = 1;
+        }
+        moveTime++;
     }
 }
