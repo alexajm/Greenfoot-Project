99c6f18f77d6423e88d8aea4645f85d3c4bf38ea
Xuerui Fa <xuerui.fa@hotmail.com>
2015-02-21 17:21:44 -0500

    Set platform for level1

--- Greenfoot-Project/GameWorld.java
+++ Greenfoot-Project/GameWorld.java
@@ -9,7 +9,7 @@ import java.util.*;
  */
 public class GameWorld extends World
 {
-    private static int level = 0;
+    private static int level = 1;
     GreenfootSound theme = new GreenfootSound("theme.mp3");
     /**
      * Constructor for objects of class GameWorld.
@@ -33,6 +33,9 @@ public class GameWorld extends World
             case 0:
                 tutorial();
                 break;
+            case 1:
+                level1();
+                break;
             default:
                 end();
                 break;
@@ -51,6 +54,47 @@ public class GameWorld extends World
         addObject(new Diamond(), 663, 359);
         addObject(new Player(), 57, 574);
         Exit.setText("Tutorial Complete");
+    }
+    public void level1() { //Object placement for Level 1
+        int height = 24;
+        int width = 72;
+        int pheight1 = 250;
+        addObject(new Player(), 91, 531);
+        addObject(new Platform(), 100, 575);
+        addObject(new Platform(), 150, 550);
+        addObject(new Platform(), 200, 525);
+        addObject(new Platform(), 350, 450);
+        addObject(new Platform(), 530, 450);
+        addObject(new Platform(), 700, 500);
+        addObject(new Platform(), 772, 500);
+        addObject(new Platform(), 844, 500);
+        addObject(new Platform(), 914, 500);
+        addObject(new Platform(), 780, 400);
+        addObject(new Water(), 250, 588);
+        addObject(new Water(), 250+width, 588);
+        addObject(new Water(), 250+(width*2), 588);
+        addObject(new Water(), 250+ (width*3), 588);
+        addObject(new Water(), 250+(width*4), 588);
+        addObject(new Water(), 250+(width*5), 588);
+        addObject(new Water(), 250+(width*6), 588);
+        addObject(new Platform(), 650, 300);
+        addObject(new Platform(), 620-width, pheight1);
+        addObject(new Platform(), 620-width*2, pheight1);
+        addObject(new Platform(), 620-width*3, pheight1);
+        addObject(new Water(), 620-width*4, pheight1);
+        addObject(new Platform(), 620-width*5, pheight1-height);
+        addObject(new Platform(), 620-width*5, pheight1-height*2);
+        addObject(new Platform(), 150, 150);
+        addObject(new Platform(), 36, 300);
+        addObject(new Platform(), 420, 120);
+        addObject(new Platform(), 420+width, 120);
+        addObject(new Platform(), 420+width*2, 120);
+        addObject(new Platform(), 420+width*3, 120);
+        addObject(new Platform(),420+width*4, 120);
+        addObject(new Exit(), 875, 250);
+        addObject(new Diamond(), 900, 460);
+        addObject(new Diamond(), 36, 260);
+        addObject(new Diamond(), 532, 410);
     }
     public void end() { //The game's end screen
         showText("You Won!", getWidth()/2, getHeight()/2);
